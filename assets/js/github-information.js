abd59d6585ed0a383dbd686f3a94e09dceb9c699
komfiGolabi <komfalos1@gmail.com>
2021-07-10 21:31:38 +0000

    added a nicer error message

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -51,6 +51,9 @@ function repoInformationHTML(repos){
 }
 function fetchGitHubInformation(event) {
 
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -74,6 +77,10 @@ function fetchGitHubInformation(event) {
              function(errorResponse){
                  if (errorResponse.status === 404){
                      $("#gh-user-data").html(`<h2>No info found for user${username}</h2>`)
+                 }else if (errorResponse.status === 403){
+                     var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                     $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
+
                  }
                  else{
                      console.log(errorResponse);
@@ -84,3 +91,4 @@ function fetchGitHubInformation(event) {
              }
         )
 }
+$(document).ready(fetchGitHubInformation);
