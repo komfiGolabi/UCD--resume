03e982d63a4429d48b164b2c3ee1a2df8fa17577
komfiGolabi <komfalos1@gmail.com>
2021-07-10 20:21:12 +0000

    fixed the bugs regarding the emoty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -27,6 +27,27 @@ function userInformationHTML(user){
         <p>Followers: ${user.followers} - Following: ${user.following}<br>Repos:${user.public_repos}
         </p>
     </diV>`;
+
+}
+
+function repoInformationHTML(repos){
+    if (repos.length ===0){
+         return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo){
+        return `<li>
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    });
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo list:</strong>
+                </p>
+                <ul>
+                  ${listItemsHTML.join("\n")}
+                </ul>
+             </div>`
 }
 function fetchGitHubInformation(event) {
 
@@ -39,13 +60,16 @@ function fetchGitHubInformation(event) {
     $("#gh-user-data").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
-        </div>`);
+        </div>`)
         $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
+            $.getJSON(`https://api.github.com/users/${username}`),
+            $.getJSON(`https://api.github.com/users/${username}/repos`)
         ).then(
-            function(response){
-                var userData = response;
+            function (firstResponse, secondResponse){
+                var userData = firstResponse[0];
+                var repoData = secondResponse[0];
                 $("#gh-user-data").html(userInformationHTML(userData))
+                $("#gh-repo-data").html(repoInformationHTML(repoData))
              },
              function(errorResponse){
                  if (errorResponse.status === 404){
